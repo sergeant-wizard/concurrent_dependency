14ad80214078f650a7797bbccde4f0f7578eeb77
Ryo Miyajima <ryo@preferred.jp>
2019-03-21 06:15:54 -0700

    convert to await at node level

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import concurrent.futures
 import time
 import typing
+import asyncio
 
 
 class Node:
     def __init__(self, name: str) -> None:
         self._name = name
         self._children = []  # type: typing.List[Node]
+        self._future = asyncio.Future()
+        self._build_started = False
 
     def add_child(self, child: "Node") -> None:
         self._children.append(child)
-
-    def __eq__(self, other: "Node") -> bool:
-        return self._name == other._name
-
-    def __hash__(self) -> int:
-        return hash(self._name)
 
     @property
     def name(self) -> str:
@@ -25,37 +22,20 @@ class Node:
     def children(self) -> typing.List["Node"]:
         return self._children
 
-    def build(self) -> "Node":
+    async def build(self, executor: concurrent.futures.Executor) -> "Node":
+        await asyncio.gather(*[
+            child.build(executor)
+            for child in self._children
+        ])
+        future = executor.submit(self._internal_build)
+        await asyncio.wrap_future(future)
+
+    def _internal_build(self) -> None:
+        if self._build_started:
+            return
+        self._build_started = True
         print(f'building {self._name}')
         time.sleep(2)
-        return self
-
-
-def build(executor: concurrent.futures.Executor, root: Node):
-    started = set()  # type: typing.Set[Node]
-    built = set()  # type: typing.Set[Node]
-
-    def get_next_task(node: Node) -> typing.Optional[Node]:
-        if node in started:
-            return None
-        elif set(node.children).issubset(built):
-            return node
-        else:
-            for child in node.children:
-                _next_task = get_next_task(child)
-                if _next_task is not None:
-                    return _next_task
-            return None
-
-    def on_done(future: concurrent.futures.Future):
-        built.add(future.result())
-
-    while root not in built:
-        next_task = get_next_task(A)
-        if next_task is not None:
-            _future = executor.submit(next_task.build)
-            started.add(next_task)
-            _future.add_done_callback(on_done)
 
 
 if __name__ == '__main__':
@@ -79,5 +59,5 @@ if __name__ == '__main__':
     C.add_child(G)
     C.add_child(H)
 
-    _executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-    build(_executor, A)
+    _executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
+    asyncio.get_event_loop().run_until_complete(A.build(_executor))
